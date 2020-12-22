de258d186bf3e2a6eaeb31ca071b9f6127ff7364
Mohammed  Etemaddar <mohammad.etemaddar@gmail.com>
2020-12-22 23:46:37 +0330

    Template tags added

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'django_hide',
     'blog.apps.BlogConfig',
 
     'taggit',
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_hide.middleware.CSRFHIDEMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
